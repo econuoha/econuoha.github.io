cc16a504d156592b474e8f58f7bb97509e59b917
econuoha <emanonuoha@gmail.com>
2021-05-15 03:22:34 -0500

    Social Bar
    Added social bar and started about me section.

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ $(document).ready(function(){
     //menu btn
     $('.menubtn').click(function(){
         $('.navibar .menu').toggleClass("active")
+        $('.socialbar a').toggleClass("gone")
+    });
+
+    $('.menu').click(function(){
+        $('.navibar .menu').toggleClass("active")
+        $('.socialbar a').toggleClass("gone")
     });
 });
