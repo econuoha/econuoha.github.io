c3124936c8c9ffd5e041a28f0321b04ecc3bf37d
econuoha <emanonuoha@gmail.com>
2021-05-16 02:51:27 -0500

    Added Languages Section.
    Added responsive cards to showcase my languages.

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ $(document).ready(function(){
         $('.navibar .menu').toggleClass("active")
 
     });
+    //lang card
+    const card1 = document.querySelector(".card__inner1");
+    card1.addEventListener("click", function (e) {
+    card1.classList.toggle('is-flipped');
+    });
+
+    const card2 = document.querySelector(".card__inner2");
+    card2.addEventListener("click", function (e) {
+    card2.classList.toggle('is-flipped');
+    });
+
+    const card3 = document.querySelector(".card__inner3");
+    card3.addEventListener("click", function (e) {
+    card3.classList.toggle('is-flipped');
+    });
+
 });
