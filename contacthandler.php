e7032acca9c9b37c73b6f88626c1f6473c91a8a0
econuoha <emanonuoha@gmail.com>
2021-05-19 02:24:19 -0500

    Update contacthandler.php

--- contacthandler.php
+++ contacthandler.php
@@ -1,18 +1,14 @@
 <?php
+    if(isset($_POST['submit'])){
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'econuoha@uh.edu';
-    $email_subject = 'New Form Submission';
-    $email_body = 'User Name: $firstname.\n'. 
-                    'User Email:$visitor_email.\n'.
-                        'User Message:$message.\n';
-    $to = 'emanonuoha@gmail.com';
-    $headers = 'From:$email_from\r\n';
-    $headers = 'Reply-To:$visitor_email\r\n';
-        mail($to, $email_subject, $email_body, $headers);
-        header(Location: contact.html);
-
-?>
+    $to = 'econuoha@uh.edu';
+    $headers = "From: "$visitor_email;
+    $txt = "You have recieved an e-mail from ".$firstname.".\n\n".$message;
+    
+    mail($to, $email_subject, $email_body, $headers);
+    header("Location: contact.html?mailsend");
+    }
