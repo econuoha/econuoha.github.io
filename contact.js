1cad0ff36127f25c88c4cca8bf35b2a2f238e7d2
econuoha <emanonuoha@gmail.com>
2021-05-28 16:12:03 -0500

    Contact Page Fixes
    Changing the contact page to work on a static website using vanilla JS.

--- contact.js
+++ contact.js
@@ -10,4 +10,16 @@ $(document).ready(function(){
         $('.navibar .menu').toggleClass("active")
 
     });
+
+    const copyText = document.querySelector("#copyText");
+    const button = document.querySelector(".btn");
+    const tooltip = document.querySelector(".tooltip");
+    button.addEventListener('click', function(){
+        copyText.select();
+        document.execCommand("copy");
+        tooltip.classList.add("show");
+        setTimeout(function(){
+            tooltip.classList.remove("show");
+        }, 700);
+    });
 });
