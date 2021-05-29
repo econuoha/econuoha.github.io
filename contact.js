a909e5bf49e8268e81e02388c4a262c644a6e8d4
econuoha <emanonuoha@gmail.com>
2021-05-28 19:09:19 -0500

    contact.js and .html
    Fixing contact scrolling for mobile.

--- contact.js
+++ contact.js
@@ -11,13 +11,17 @@ $(document).ready(function(){
 
     });
 
+    const focusMethod = function getFocus(){
+        document.getElementById("myButton").focus();
+    }
+
     const copyText = document.querySelector("#copyText");
     const button = document.querySelector(".btn");
     const tooltip = document.querySelector(".tooltip");
     button.addEventListener('click', function(){
         copyText.select();
         document.execCommand("copy");
-        button.focus();
+        focusMethod();
         tooltip.classList.add("show");
         setTimeout(function(){
             tooltip.classList.remove("show");
