e878069d2b42608ae1f09301bb45d4f9964964c8
econuoha <emanonuoha@gmail.com>
2021-05-31 17:25:32 -0500

    Created Timeline + Read more
    Created my timeline and implemented proper read more functionality.

--- main.js
+++ main.js
@@ -26,4 +26,50 @@ $(document).ready(function(){
     card3.classList.toggle('is-flipped');
     });
 
+    
+    // read more buttons for my timeline
+    let blur = document.getElementById('blur');
+    let pop = document.getElementById('popup');
+    let pop2 = document.getElementById('popup2');
+    let pop3 = document.getElementById('popup3');
+    let pop4 = document.getElementById('popup4');
+    let pop5 = document.getElementById('popup5');
+    $('.bottom a').click(function(){
+        blur.classList.toggle('active');
+    });
+    $('#firstRead').click(function(){
+        pop.classList.toggle('active');
+    });
+    $('#firstPop').click(function(){
+        blur.classList.toggle('active');
+        pop.classList.toggle('active');
+    });
+    $('#secondRead').click(function(){
+        pop2.classList.toggle('active');
+    });
+    $('#secondPop').click(function(){
+        blur.classList.toggle('active');
+        pop2.classList.toggle('active');
+    });
+    $('#thirdRead').click(function(){
+        pop3.classList.toggle('active');
+    });
+    $('#thirdPop').click(function(){
+        blur.classList.toggle('active');
+        pop3.classList.toggle('active');
+    });
+    $('#fourthRead').click(function(){
+        pop4.classList.toggle('active');
+    });
+    $('#fourthPop').click(function(){
+        blur.classList.toggle('active');
+        pop4.classList.toggle('active');
+    });
+    $('#fifthRead').click(function(){
+        pop5.classList.toggle('active');
+    });
+    $('#fifthPop').click(function(){
+        blur.classList.toggle('active');
+        pop5.classList.toggle('active');
+    });
 });
