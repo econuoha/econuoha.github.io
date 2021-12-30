8773c8d7f206c99e492a9d1094f96bd51d49254d
econuoha <emanonuoha@gmail.com>
2021-12-30 00:58:53 -0600

    Fixed timeline, QOTM, Spotify, added py card, added new feature to about me, changed homescreen background

--- main.js
+++ main.js
@@ -26,7 +26,57 @@ $(document).ready(function(){
     card3.classList.toggle('is-flipped');
     });
 
-    
+    const card4 = document.querySelector(".card__inner4");
+    card4.addEventListener("click", function (e) {
+    card4.classList.toggle('is-flipped');
+    });
+
+    // Get all the images
+    image_array = [
+    'all images\\IMG-3088.JPG',
+    'all images\\96C06788-0D72-42F7-B682-ED3F66D2F6BB.JPG',
+    'all images\\669442A7-A7E1-4243-887D-A2A520D468CE.JPG',
+    'all images\\C605DC33-BC37-4692-8FD5-4C66241ED64A.JPG',
+    'all images\\D0B9A579-83FB-4A22-8444-A38EFF03CEA6.JPG',
+    'all images\\DSC02817.JPEG',
+    'all images\\DSC02867.JPEG',
+    'all images\\DSC02912.JPEG',
+    'all images\\DSC02983.JPEG',
+    'all images\\DSC03022.JPEG',
+    'all images\\F44D863F-2D7F-4CD4-A200-EA6219BB817F.JPG',
+    'all images\\fb.jpg',
+    'all images\\IMG_2317.JPG',
+    'all images\\IMG_3577.JPG',
+    'all images\\IMG-3088.JPG',
+    'all images\\64608687510__5D94BA21-EA01-46B7-B75C-1147234B09FE.HEIC.jpg',
+    'all images\\IMG_9472.HEIC.jpg',
+    'all images\\IMG_7098.HEIC.jpg',
+    'all images\\IMG_7068.HEIC.jpg',
+    'all images\\IMG_7056.HEIC.jpg',
+    'all images\\IMG_7053.HEIC.jpg',
+    'all images\\IMG_6897.HEIC.jpg',
+    'all images\\IMG_6309.HEIC.jpg',
+    'all images\\IMG_6283.HEIC.jpg',
+    'all images\\IMG_6253.HEIC.jpg',
+    'all images\\IMG_6139.HEIC.jpg',
+    'all images\\IMG_5834.HEIC.jpg',
+    'all images\\IMG_5805.HEIC.jpg',
+    'all images\\IMG_5453.HEIC.jpg',
+    'all images\\IMG_5452.HEIC.jpg',
+    'all images\\IMG_0663.HEIC.jpg',
+    'all images\\IMG_0661.HEIC.jpg',
+    'all images\\IMG_0423.HEIC.jpg',
+    ]
+    const randPhoto = document.getElementById('button_container');
+    randPhoto.addEventListener("click", function(e){
+        random_index = Math.floor(Math.random() * image_array.length);
+        selected_image = image_array[random_index]
+        // Display the image
+        document.getElementById('myPhoto').src = `${selected_image}`
+    });
+
+
+  
     // read more buttons for my timeline
     let blur = document.getElementById('blur');
     let pop = document.getElementById('popup');
